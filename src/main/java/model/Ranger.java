89054cd7adde989d8ccf8f0b7908e87689ecd97d
Simoncsik András <m8kk57@inf.elte.hu>
2025-03-25 14:53:11 +0100

    Entity adattagok/metódusok

--- src/main/java/model/Ranger.java
+++ src/main/java/model/Ranger.java
@@ -1,9 +1,19 @@
 package model;
 
-public class Ranger {
+public class Ranger extends Entity{
     private double salary;
 
     public void stopPoacher(Poacher poacher) {
         // Orvvadász megállítása
+    }
+
+    @Override
+    public void moveTo(Coordinate target) {
+
+    }
+
+    @Override
+    public void move() {
+
     }
 }
