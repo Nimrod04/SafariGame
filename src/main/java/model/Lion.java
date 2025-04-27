dc48aafa62c6626a685497f6a9bccbff12ac2503
Simoncsik András <m8kk57@inf.elte.hu>
2025-04-27 22:54:57 +0200

    animals

--- src/main/java/model/Lion.java
+++ src/main/java/model/Lion.java
@@ -12,10 +12,6 @@ public class Lion extends Carnivore {
 
     public static final int PRICE = 2000;
 
-    public static final int PRICE = 2000;
-
-
-
     @Override
     public int getPrice() {
         return PRICE;
